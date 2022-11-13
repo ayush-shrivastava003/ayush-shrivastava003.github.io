f621eea55f9b05fe7d85cd8277a185de0879be00
moistpotato9873 <77131808+ayush-shrivastava003@users.noreply.github.com>
2022-11-12 23:38:39 -0500

    fix js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let songs = JSON.parse(await fetch("./tracks.json"))
+let songs = await fetch("./tracks.json").then((res) => res.json())
 let songIcons = document.getElementsByClassName("song-img")
 
 for (let i = 0; i < songs.length; i++) {
     console.log(songs)
     console.log(songIcons)
-    songIcons[0].src = songs[0].img
+    songIcons[0].src = songs[0].img // TODO: show song name & artist when hovering
 }
