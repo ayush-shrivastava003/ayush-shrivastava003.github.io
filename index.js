24464abe811a7d4c861c2f98692ddf3264367e72
ayush-shrivastava003 <77131808+ayush-shrivastava003@users.noreply.github.com>
2024-04-30 11:25:28 -0400

    changing things up

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-let songs = await fetch("./tracks.json").then((res) => res.json())
-let songIcons = document.getElementsByClassName("song-img")
-let songUrls = document.getElementsByClassName("song-link")
-
 // window.addEventListener("resize", (e) => console.log("d", e));
 let name = document.getElementById("name")
-let underline = document.getElementById("underline")
 let splashImg = document.getElementById("splash-img")
-let blurb = document.getElementById("splash-blurb")
 
 let a = document.getElementById("work-item-a")
 let works = await fetch("./works.json").then((res) => res.json())
@@ -16,16 +10,12 @@ function run() {
     function cb(entries) {
         entries.forEach((entry) => {
             let elem = entry.target
-            console.log(elem, elem.className)
             if (entry.isIntersecting) {
                 if (elem.className == "name") splashImg.style.filter = "brightness(30%) grayscale(100%)";
                 else elem.style.animation = "fadein 0.75s ease-in-out 0s forwards"
             } else {
-                if (elem.className == "name") splashImg.style.filter = "brightness(15%) grayscale(100%)";
-                else {
                     elem.style.animation = ""
                     elem.style.opacity = "0%"
-                }
             }
         })
     }
@@ -59,52 +49,92 @@ if (document.readyState !== "loading") {
 } else window.addEventListener("DOMContentLoaded", run)
 
 const nameContainer = document.getElementById("name-container")
-let trait1 = document.getElementById("trait1")
-let trait2 = document.getElementById("trait2")
-let trait3 = document.getElementById("trait3")
+// const seconds = document.getElementById("seconds")
+// const minutes = document.getElementById("minutes")
+// const hours = document.getElementById("hours")
 let scroll = 0
-const t1Unit = 103/830
-const t3Unit = 88/830 // in %
-const t2Unit = -70/830
-const duration = 200
 let vpHeight = window.innerHeight;
-console.log(nameContainer.scrollHeight * 2)
+// console.log(nameContainer.scrollHeight * 2)
+
+// for (let i = 0; i < 6; i++) {
+//     let s = document.createElement("p")
+//     s.innerText = "."
+//     seconds.appendChild(s)
+
+//     let m = document.createElement("p")
+//     m.innerText = "."
+//     minutes.appendChild(m)
+
+//     let h = document.createElement("p")
+//     h.innerText = "."
+//     hours.appendChild(h)
+// }
+
+// for (let i = 0; i < 6; i++) {
+//     let h = document.createElement("p")
+//     h.innerText = "."
+//     hours.appendChild(h)
+// }
 
 window.addEventListener('wheel', (e) => {
     scroll = Math.min(nameContainer.scrollHeight * 4, Math.max(scroll + e.deltaY, 0));
     let ncHeight = nameContainer.getBoundingClientRect().bottom
     let progress = Math.min(1, Math.max(0, 1 - ((scroll - (0.15 * vpHeight)) / ncHeight)));
-    console.log(scroll, ncHeight, scroll / ncHeight, progress, window.innerHeight - ncHeight);
     nameContainer.style.opacity = progress;
-    // splashImg.style.filter = `brightness(${Math.min(15, progress * 30)}%) grayscale(100%)`
-    
-    let t1Left = t1Unit * Math.max(scroll - 125, 0);
-    let t3Left = t3Unit * Math.max(scroll - 125, 0);
-    let t2Left = t2Unit * Math.max(scroll - 125, 0);
-    // // console.log(left);
-    trait1.animate(
-        [{transform: `translate(${t1Left}%, 85%)`}], {
-        duration,
-        iterations: 1,
-        ease: "ease-in-out"
-    })
-
-    trait2.animate(
-        [{transform: `translate(${t2Left}%, 20%`}], {
-        duration,
-        iterations: 1,
-        ease: "ease-in-out"
-    })
-
-    trait3.animate(
-        [{transform: `translate(${t3Left}%,-45%`}], {
-        duration,
-        iterations: 1,
-        ease: "ease-in-out"
-    })
-
-    trait1.style.transform = `translate(${t1Left}%,85%)`
-    trait2.style.transform = `translate(${t2Left}%, 20%)`
-    trait3.style.transform = `translate(${t3Left}%,-45%)`
 }, {passive: false})
 
+const dateTime = document.getElementById("date-time")
+dateTime.innerText = new Date().toLocaleDateString(
+    'en-us', {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "2-digit"
+    }).toUpperCase()
+
+// let d = new Date()
+// let s = d.getSeconds();
+// s = ((s - (s % 10)) / 10)
+// let m = d.getMinutes();
+// m = ((m - (s % 10)) / 10)
+// let h = d.getHours();
+// h = ((h > 11) ? h - 12: h)
+// console.log(h)
+
+// function updateSeconds() {
+//     if (s > 6) {
+//         for (let i = 0; i < s; i++) seconds.childNodes[i].style = "";
+//         s = 0;
+//         return;
+//     }
+
+//     for (let i = 0; i < s; i++) seconds.childNodes[i].style = "font-weight: bold;";
+//     s++;
+// }
+
+// function updateMinutes() {
+//     if (m > 6) {
+//         for (let i = 0; i < m; i++) minutes.childNodes[i].style = "";
+//         m = 0;
+//         return;
+//     }
+
+//     for (let i = 0; i < m; i++) minutes.childNodes[i].style = "font-weight: bold;";
+//     m++;
+// }
+
+// function updateHours() {
+//     if (h > 11) {
+//         for (let i = 0; i < h; i++) hours.childNodes[i].style = "";
+//         h = 0;
+//         return;
+//     }
+
+//     for (let i = 0; i < h; i++) hours.childNodes[i].style = "font-weight: bold;";
+//     h++;
+// }
+
+// updateSeconds(); updateMinutes(); updateHours();
+// setInterval(updateSeconds, 10000)
+// setInterval(updateMinutes, 60000)
+// setInterval(updateHours, 3600000)
