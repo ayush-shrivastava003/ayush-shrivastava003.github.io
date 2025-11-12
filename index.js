910cb7b13025a739e3c3e0005e3f992f7b87fbc8
ayush-shrivastava003 <77131808+ayush-shrivastava003@users.noreply.github.com>
2025-11-12 18:05:21 -0500

    new fonts, video hero

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // window.addEventListener("resize", (e) => console.log("d", e));
-let name = document.getElementById("name")
+let name = document.getElementsByClassName("name")[0]
 let splashImg = document.getElementById("splash-img")
 
 let a = document.getElementById("work-item-a")
@@ -11,7 +11,7 @@ function run() {
         entries.forEach((entry) => {
             let elem = entry.target
             if (entry.isIntersecting) {
-                if (elem.className == "name") splashImg.style.filter = "brightness(30%) grayscale(100%)";
+                if (elem.className == "name") return;
                 else elem.style.animation = "fadein 0.75s ease-in-out 0s forwards"
             } else {
                     elem.style.animation = ""
@@ -49,38 +49,16 @@ if (document.readyState !== "loading") {
 } else window.addEventListener("DOMContentLoaded", run)
 
 const nameContainer = document.getElementById("name-container")
-// const seconds = document.getElementById("seconds")
-// const minutes = document.getElementById("minutes")
-// const hours = document.getElementById("hours")
 let scroll = 0
 let vpHeight = window.innerHeight;
-// console.log(nameContainer.scrollHeight * 2)
 
-// for (let i = 0; i < 6; i++) {
-//     let s = document.createElement("p")
-//     s.innerText = "."
-//     seconds.appendChild(s)
-
-//     let m = document.createElement("p")
-//     m.innerText = "."
-//     minutes.appendChild(m)
-
-//     let h = document.createElement("p")
-//     h.innerText = "."
-//     hours.appendChild(h)
-// }
-
-// for (let i = 0; i < 6; i++) {
-//     let h = document.createElement("p")
-//     h.innerText = "."
-//     hours.appendChild(h)
-// }
 
 window.addEventListener('wheel', (e) => {
     scroll = Math.min(nameContainer.scrollHeight * 4, Math.max(scroll + e.deltaY, 0));
     let ncHeight = nameContainer.getBoundingClientRect().bottom
     let progress = Math.min(1, Math.max(0, 1 - ((scroll - (0.15 * vpHeight)) / ncHeight)));
     nameContainer.style.opacity = progress;
+    name.style.opacity = progress;
 }, {passive: false})
 
 const dateTime = document.getElementById("date-time")
@@ -92,49 +70,10 @@ dateTime.innerText = new Date().toLocaleDateString(
         day: "2-digit"
     }).toUpperCase()
 
-// let d = new Date()
-// let s = d.getSeconds();
-// s = ((s - (s % 10)) / 10)
-// let m = d.getMinutes();
-// m = ((m - (s % 10)) / 10)
-// let h = d.getHours();
-// h = ((h > 11) ? h - 12: h)
-// console.log(h)
-
-// function updateSeconds() {
-//     if (s > 6) {
-//         for (let i = 0; i < s; i++) seconds.childNodes[i].style = "";
-//         s = 0;
-//         return;
-//     }
-
-//     for (let i = 0; i < s; i++) seconds.childNodes[i].style = "font-weight: bold;";
-//     s++;
-// }
-
-// function updateMinutes() {
-//     if (m > 6) {
-//         for (let i = 0; i < m; i++) minutes.childNodes[i].style = "";
-//         m = 0;
-//         return;
-//     }
-
-//     for (let i = 0; i < m; i++) minutes.childNodes[i].style = "font-weight: bold;";
-//     m++;
-// }
-
-// function updateHours() {
-//     if (h > 11) {
-//         for (let i = 0; i < h; i++) hours.childNodes[i].style = "";
-//         h = 0;
-//         return;
-//     }
-
-//     for (let i = 0; i < h; i++) hours.childNodes[i].style = "font-weight: bold;";
-//     h++;
-// }
-
-// updateSeconds(); updateMinutes(); updateHours();
-// setInterval(updateSeconds, 10000)
-// setInterval(updateMinutes, 60000)
-// setInterval(updateHours, 3600000)
+let videoIdx = 0;
+splashImg.addEventListener("ended", () => {
+    videoIdx == 1 ? videoIdx = 0 : videoIdx++;
+    splashImg.src = `nyc${videoIdx}.mp4`;
+    splashImg.load();
+    splashImg.play();
+}, false)
